c382107f5baaca5e3abd1cc644eab98faeb30db1
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2021-03-08 20:53:30 -0700

    Initial Code for project.

--- src/main/java/edu/uci/ics/cse223/MainDemo.java
+++ src/main/java/edu/uci/ics/cse223/MainDemo.java
@@ -1,13 +1,31 @@
 package edu.uci.ics.cse223;
 
+import java.sql.*;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class MainDemo
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws SQLException {
         System.out.println( "Hello World!" );
+        String url = "jdbc:postgresql://localhost/testdump";
+        Properties props = new Properties();
+        props.setProperty("user","postgres");
+        props.setProperty("password","postgres");
+        props.setProperty("ssl","false");
+        Connection conn = DriverManager.getConnection(url, props);
+
+//        String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+//        Connection conn = DriverManager.getConnection(url);
+
+        PreparedStatement result = conn.prepareStatement("SELECT COUNT(*) FROM thermometerobservation;");
+        result.execute();
+        ResultSet rs = result.getResultSet();
+        while (rs.next()) {
+            System.out.println(rs.getInt(1));
+        }
     }
 }
---
+++ src/main/java/edu/uci/ics/cse223/TwoPCAgent.java
@@ -0,0 +1,8 @@
+package edu.uci.ics.cse223;
+
+public class TwoPCAgent {
+
+    public static void main(String[] args) {
+
+    }
+}
