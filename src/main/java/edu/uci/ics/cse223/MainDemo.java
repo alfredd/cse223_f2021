81ded234a9a0df5e6d8774a96ac545c83b2de9f8
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2021-03-08 17:55:55 -0700

    Initial Code

---
+++ src/main/java/edu/uci/ics/cse223/MainDemo.java
@@ -0,0 +1,13 @@
+package edu.uci.ics.cse223;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainDemo
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
